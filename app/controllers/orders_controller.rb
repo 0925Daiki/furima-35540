310ab1b64679fd8c1135edda5fbe68868eb37deb
0925Daiki <82555115+0925Daiki@users.noreply.github.com>
2021-05-27 20:30:49 +0900

    AWS　デプロイ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,7 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
   end
-
+  
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
@@ -17,13 +17,13 @@ class OrdersController < ApplicationController
       render :index
     end
   end
-
+  
   private
-
+  
   def order_params
     params.require(:order_address).permit(:postal_code, :shipping_area_id, :municipality, :city, :building_name, :phone).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
-
+  
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
@@ -32,15 +32,16 @@ class OrdersController < ApplicationController
       currency: 'jpy'
     )
   end
-
+  
   def set_item
     @item = Item.find(params[:item_id])
   end
-
+  
   def move_to_index
     if current_user.id == @item.user_id ||  @item.order != nil
       redirect_to root_path
     end
   end
-
+  
 end
+
